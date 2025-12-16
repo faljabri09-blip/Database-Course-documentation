f63441838af96e99580044889c7a72ef134d5ccf
faljabri09-blip <faljabri09@gmail.com>
2025-12-16 16:22:20 +0400

    Adding files

---
+++ SQL Query/College/DQL + DML Query.sql
@@ -0,0 +1,19 @@
+---Q1
+
+select * from Student
+
+---Q2
+
+select FName, LName as [fall name] 
+from Student
+
+---Q3
+
+SELECT COUNT(*) FROM Student
+
+---Q4
+
+select FName
+from Student
+WHERE FName LIKE '%a' 
+
